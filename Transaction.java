d25b8571992ffd963b938cdf1f61c7e21318c5b3
01akashaksh <greedy.akash@gmail.com>
2024-12-01 16:25:42 -0500

    Task 2 Part 2
    Implemented file-based storage for transactions.
    Added the saveTransaction(String transactionDetails) method inside the Transaction class, so every borrowing and returning transaction will be saved into a transactions.txt file. borrowBook and returnBook methods updated with a call to this method to persist the history of transactions in case of program exit.

--- Transaction.java
+++ Transaction.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -26,6 +28,9 @@ public class Transaction {
             member.borrowBook(book); 
             String transactionDetails = getCurrentDateTime() + " - Borrowing: " + member.getName() + " borrowed " + book.getTitle();
             System.out.println(transactionDetails);
+            // Task 2.2
+            //Save to file
+            saveTransaction(transactionDetails);
             return true;
         } else {
             System.out.println("The book is not available.");
@@ -41,6 +46,9 @@ public class Transaction {
             book.returnBook();
             String transactionDetails = getCurrentDateTime() + " - Returning: " + member.getName() + " returned " + book.getTitle();
             System.out.println(transactionDetails);
+         // Task 2.2
+            //Save to file
+            saveTransaction(transactionDetails);
         } else {
             System.out.println("This book was not borrowed by the member.");
         }
@@ -52,4 +60,19 @@ public class Transaction {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return sdf.format(new Date());
     }
+    
+    // Task 2.2
+    // Save transaction details in a file  when  the  program  exits,  from  borrowing  and  returning  transactions
+    private void saveTransaction(String transactionDetails)
+    {
+    	try(FileWriter writer = new FileWriter("transactions.txt",true))
+    	{
+    		writer.write(transactionDetails + "\n");
+    	}
+    	catch(IOException e)
+    	{
+    		System.err.println("Error saving transaction:(" + e.getMessage());
+    	}
+    }
+
 }
